f3cd77ea813362c98015cbc72bdfca0edd12bfc7
John Robert Thurlby <johnrthurlby@gmail.com>
2018-07-23 07:43:50 -0400

    Latest code, rideinfo

--- models/index.js
+++ models/index.js
@@ -4,16 +4,11 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-
-console.log("base " + basename)
-console.log("in index.js in /models")
-
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-console.log(config)
+var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
+
 if (process.env.use_env_variable) {
-  //var sequelize = new Sequelize(process.env[config.use_env_variable]);
   var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
